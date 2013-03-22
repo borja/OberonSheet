e7c85a2f2c8b186228439eafc7729a7cb96fd59d
fgendc <fernando.gento@gmail.com>
2013-03-22 15:22:29 +0100

    Removed tab indentation
    Replaced tab indentation for 4-space indentation

--- index.py
+++ index.py
@@ -1,14 +1,12 @@
-import bottle
-
-import sys
+from bottle import route, run, template
 
 @bottle.route('/')
 def index():
-	print "Entra alguien!"
-	return bottle.template("home")
+print "Entra alguien!"
+    return bottle.template("home")
 
 @bottle.route('/static/:path#.+#', name='static')
 def static(path):
-	return bottle.static_file(path, root='static')
+    return bottle.static_file(path, root='static')
 
 bottle.run(host='localhost', port=8082, debug=True)
